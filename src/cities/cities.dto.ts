98266103a099420474ce0c0b8b0211b553619b2d
unknown <josutic.sutic@gmail.com>
2025-03-17 19:50:45 +0100

    feat(15): swagger add cities dto

--- src/cities/cities.dto.ts
+++ src/cities/cities.dto.ts
@@ -1,13 +1,66 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNumber, IsOptional } from "class-validator";
+
 export class CreateCityDto {
-    id?: number;
-    name: string;
-    country: string;
-    numbeo_id: number;
+  @ApiProperty({
+    description: "The unique identifier of the city",
+    required: false, // The id is optional in CreateCityDto
+    type: Number,
+  })
+  @IsOptional()
+  @IsNumber()
+  id?: number;
+
+  @ApiProperty({
+    description: "The name of the city",
+    type: String,
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: "The country where the city is located",
+    type: String,
+  })
+  @IsString()
+  country: string;
+
+  @ApiProperty({
+    description: "The numbeo ID for the city",
+    type: Number,
+  })
+  @IsNumber()
+  numbeo_id: number;
 }
 
 export class CityDto {
-    id: number;
-    name: string;
-    country: string;
-    numbeo_id: number;
+  @ApiProperty({
+    description: "The unique identifier of the city",
+    required: true,
+    type: Number,
+  })
+  @IsOptional()
+  @IsNumber()
+  id: number;
+
+  @ApiProperty({
+    description: "The name of the city",
+    type: String,
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: "The country where the city is located",
+    type: String,
+  })
+  @IsString()
+  country: string;
+
+  @ApiProperty({
+    description: "The numbeo ID for the city",
+    type: Number,
+  })
+  @IsNumber()
+  numbeo_id: number;
 }
